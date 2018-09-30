8b9e88e7fae845593b82843accbc335db4201303
Mahmoud <sbs8@yahoo.com>
2018-09-30 18:42:06 +0330

    اصلاح روز کاری اصلاح ساختار کدها تغییر کوچک در ظاهر فرم

--- append_view.js
+++ append_view.js
@@ -1,3 +1,6 @@
+/**
+ * Convert Persian Digits to English Digits
+ */
 function toEnglish(val) {
     var num_dic = {
         '۰': '0',
@@ -16,6 +19,9 @@ function toEnglish(val) {
     }));
 }
 
+/**
+ * Convert English Digits to Persian Digits
+ */
 function toPersian(val) {
     var num_dic = {
         '0': '۰',
@@ -45,40 +51,24 @@ function toPersian(val) {
  * @returns {string}
  * @constructor
  */
-TomanToHour = function (Toman, Rate, DailyHours, Daily) {
+LifeTimeCalculator = function (Toman, Unit, Rate, DailyHours, Daily) {
     if (Toman.indexOf(',') < 0) {
         return Toman;
     }
     price = toEnglish(Toman.replace(/[, ]/g, ''));
+    if (Unit === "Rial") {
+        price /= 10;
+    }
+
     hour = Math.floor(price / Rate);
     minute = (price / Rate) - hour;
     if (Daily == 1) {
-        return toPersian(Math.round((price / Rate)/DailyHours * 100)/100) + "عمر";    
+        return toPersian(Math.round((price / Rate) / DailyHours * 100) / 100) + "روز";
     }
-    return toPersian(hour) + ":" + toPersian(Math.round(minute * 60))+ "عمر";
+    return toPersian(hour) + ":" + toPersian(Math.round(minute * 60)) + "ساعت کار";
 };
 
-/**
- *
- * @param Rial
- * @param Rate
- * @returns {string}
- * @constructor
- */
-RialToHour = function (Rial, Rate, DailyHours, Daily) {
-    if (Rial.indexOf(',') < 0) {
-        return Rial;
-    }
-    price = parseInt(toEnglish(Rial.replace(/[, ]/g, '')))/10;
-    hour = Math.floor(price / Rate);
-    minute = (price / Rate) - hour;
-    if (Daily == 1) {
-        return toPersian(Math.round((price / Rate)/DailyHours * 100)/100) + "عمر";    
-    }
-    return toPersian(hour) + ":" + toPersian(Math.round(minute * 60))+ "عمر";
-};
-
-ConvertTohourly = function () {
+CalculateLifeTime = function () {
     chrome.storage.sync.get(['hourly_wages', 'daily_hours', 'is_active', 'daily'], function (result) {
         if (result.is_active !== undefined && result.is_active === 0) {
             return false;
@@ -93,23 +83,23 @@ ConvertTohourly = function () {
 
                 //Ware pages
                 $('.js-price-value,del').each(function () {
-                    $(this).html(TomanToHour($(this).html(), result.hourly_wages, result.daily_hours, result.daily));
+                    $(this).html(LifeTimeCalculator($(this).html(), "Toman", result.hourly_wages, result.daily_hours, result.daily));
                 });
 
                 //Sub Slider
                 $('.c-price__value:not(.js-variant-price)').each(function () {
-                    $(this).html(TomanToHour($(this).html(), result.hourly_wages, result.daily_hours, result.daily));
+                    $(this).html(LifeTimeCalculator($(this).html(), "Toman", result.hourly_wages, result.daily_hours, result.daily));
                 });
 
                 //Main Slider
                 $('.c-discount__price--original,.c-discount__price--now').each(function () {
                     $(this).find('span').remove();
-                    $(this).html(TomanToHour($(this).text(), result.hourly_wages, result.daily_hours, result.daily));
+                    $(this).html(LifeTimeCalculator($(this).text(), "Toman", result.hourly_wages, result.daily_hours, result.daily));
                 });
 
                 //Special Slider
                 $('.c-promo-single__price,.c-promo-single__discount').each(function () {
-                    $(this).html(TomanToHour($(this).text(), result.hourly_wages, result.daily_hours, result.daily));
+                    $(this).html(LifeTimeCalculator($(this).text(), "Toman", result.hourly_wages, result.daily_hours, result.daily));
                 });
 
                 //Clear Toman
@@ -126,20 +116,20 @@ ConvertTohourly = function () {
                 setTimeout(function () {
                     //Group 1
                     $('[data-price],._6oBb.aVuo._3QXu.aiK9,._22Qe._9JX9._3Fln.e4Ka._7ppA').each(function () {
-                        $(this).html(RialToHour($(this).html(), result.hourly_wages, result.daily_hours, result.daily));
+                        $(this).html(LifeTimeCalculator($(this).html(), "Rial", result.hourly_wages, result.daily_hours, result.daily));
                     });
                     //Clear Rial
                     $('[data-currency-iso],span').each(function () {
-                        if($(this).html() == 'ریال'){
+                        if ($(this).html() == 'ریال') {
                             $(this).remove()
                         }
-                        if($(this).html().indexOf(',') > -1){
-                            if(toEnglish($(this).html()) > 0){
-                                $(this).html(RialToHour($(this).html(), result.hourly_wages, result.daily_hours, result.daily));
+                        if ($(this).html().indexOf(',') > -1) {
+                            if (toEnglish($(this).html()) > 0) {
+                                $(this).html(LifeTimeCalculator($(this).html(), "Rial", result.hourly_wages, result.daily_hours, result.daily));
                             }
                         }
                     });
-                },4000);
+                }, 4000);
 
             }
 
@@ -149,16 +139,16 @@ ConvertTohourly = function () {
 $(document).ready(function () {
 
     $('body').delegate('[data-event=config_change]', 'click', function () {
-        setTimeout(ConvertTohourly, 1200)
+        setTimeout(CalculateLifeTime, 1200)
     });
     $('body').delegate('.c-pager__item', 'click', function () {
-        setTimeout(ConvertTohourly, 1200)
+        setTimeout(CalculateLifeTime, 1200)
     });
-	
-	if (window.location.href.indexOf('www.bamilo.com') > -1) {
-		$('body').delegate('span,a','click',function () {
-			setTimeout(ConvertTohourly, 1200)
-		});
-	}
-    setTimeout(ConvertTohourly, 1200)
+
+    if (window.location.href.indexOf('www.bamilo.com') > -1) {
+        $('body').delegate('span,a', 'click', function () {
+            setTimeout(CalculateLifeTime, 1200)
+        });
+    }
+    setTimeout(CalculateLifeTime, 1200)
 });
