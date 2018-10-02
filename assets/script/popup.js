89de01d3b6f66c4c51af15de25a0c5b5030860a1
Mahmoud <sbs8@yahoo.com>
2018-10-02 13:17:24 +0330

    Add Supported websites ltr input fields

--- assets/script/popup.js
+++ assets/script/popup.js
@@ -9,12 +9,16 @@ var PopupController = function () {
     this.addListeners_();
 };
 
-function sepereteDigit(_number ){
+/**
+ * Comma seperator
+ * @param _number
+ * @returns {string}
+ */
+function SeparateDigit(_number) {
     var input = _number.toString().replace(/[\D\s\._\-]+/g, "");
-    input = input ? parseInt( input, 10 ) : 0;
-    return ( input === 0 ) ? "" : input.toLocaleString("en-US") ;
-}// @function: sepereteDigit()
-
+    input = input ? parseInt(input, 10) : 0;
+    return (input === 0) ? "" : input.toLocaleString("en-US");
+}
 
 
 PopupController.prototype = {
@@ -28,28 +32,28 @@ PopupController.prototype = {
         this.daily_hours.addEventListener('change', this.handledaily_hours_.bind(this));
         this.switch_.addEventListener('change', this.handleSwitch_.bind(this));
         this.daily_.addEventListener('change', this.handleDaily_.bind(this));
-        this.hourly_wages.addEventListener('keyup',this.handlehourly_wages_digit.bind(this))
+        this.hourly_wages.addEventListener('keyup', this.handlehourly_wages_digit.bind(this))
     },
 
-    handlehourly_wages_digit: function(_el){
+    handlehourly_wages_digit: function (_el) {
         var val = _el.srcElement.value;
-        _el.srcElement.value = sepereteDigit(val);
+        _el.srcElement.value = SeparateDigit(val);
     },
 
     /**
      *
      * @private
-     */
-    handleCallback_: function () {
+     * @depercated
+     * handleCallback_: function () {
         console.log('handleCallback_');
-    },
+    },*/
 
     /**
      *
      * @private
      */
     handlehourly_wages_: function (_el) {
-        chrome.storage.sync.set({hourly_wages: _el.srcElement.value.replace(/[\D\s\._\-]+/g, "") });
+        chrome.storage.sync.set({hourly_wages: _el.srcElement.value.replace(/[\D\s\._\-]+/g, "")});
     },
 
     /**
@@ -78,19 +82,17 @@ PopupController.prototype = {
 };
 
 document.addEventListener('DOMContentLoaded', function () {
-
     chrome.storage.sync.get(['hourly_wages', 'daily_hours', 'is_active', 'daily'], function (result) {
         document.getElementById('switch').checked = result.is_active !== 0;
         document.getElementById('daily').checked = result.daily !== 0;
-
         if (result.daily === undefined) {
             chrome.storage.sync.set({daily: true});
         }
         if (result.hourly_wages === undefined) {
             chrome.storage.sync.set({hourly_wages: 12000});
-            document.getElementById('hourly_wages').value = sepereteDigit( 12000 );
+            document.getElementById('hourly_wages').value = SeparateDigit(12000);
         } else {
-            document.getElementById('hourly_wages').value = sepereteDigit( result.hourly_wages );
+            document.getElementById('hourly_wages').value = SeparateDigit(result.hourly_wages);
         }
 
         if (result.daily_hours === undefined) {
@@ -99,38 +101,29 @@ document.addEventListener('DOMContentLoaded', function () {
         } else {
             document.getElementById('daily_hours').value = result.daily_hours;
         }
-
-
     });
-
-
-
- 
-
-
-
-
-
     window.PC = new PopupController();
 });
 
 
-
-chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
-    if(
-        tabs[0].url.indexOf("https://www.digikala.com/") > -1 ||
-        tabs[0].url.indexOf("https://www.bamilo.com/") > -1 ||
-        tabs[0].url.indexOf("https://www.reyhoon.com/") > -1 ||
-        tabs[0].url.indexOf("https://emalls.ir/") > -1 ||
-        tabs[0].url.indexOf("https://torob.com/") > -1 ||
-        tabs[0].url.indexOf("https://snappfood.ir/") > -1  
-    ){
+chrome.tabs.query({currentWindow: true, active: true}, function (tabs) {
+    var SupportedWebsites = [
+        "https://www.digikala.com/",
+        "https://www.bamilo.com/",
+        "https://www.reyhoon.com/",
+        "https://emalls.ir/",
+        "https://torob.com/",
+        "https://snappfood.ir/"
+    ];
+    if (SupportedWebsites.indexOf(tabs[0].url) > -1) {
         document.querySelector(".header").classList.add("active");
         document.querySelector("._blank").classList.add("active");
+        document.querySelector(".btn_supported").classList.remove("active");
         document.querySelector(".input_wrap.form").classList.remove("_blure");
-    }else{
+    } else {
         document.querySelector(".header").classList.remove("active");
         document.querySelector("._blank").classList.remove("active");
+        document.querySelector(".btn_supported").classList.add("active");
         document.querySelector(".input_wrap.form").classList.add("_blure");
     }
 });
